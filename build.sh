164a9b3d32b4ac672d808cf3703b706c9de27a91
hertz <georgedot@gmail.com>
2020-06-19 20:15:42 +0200

    second try

--- build.sh
+++ build.sh
@@ -1,22 +1,39 @@
 #!/usr/bin/env bash
 
-set -e
+set -xe
 
 ORIGIN="verdaccio/verdaccio"
 MIRROR="hertzg/verdaccio"
 PLATFORMS="linux/amd64,linux/386,linux/arm64,linux/ppc64le,linux/arm/v7,linux/arm/v6"
 
-TOSYNC=$(node ./tagsToSync.js "$ORIGIN" "$MIRROR" | uniq)
-TOSYNC="latest $TOSYNC"
-for tag in $TOSYNC; do
+BUILDMAP=$(node ./buildMap.js "$ORIGIN" "$MIRROR")
+
+if [ ! -d './verdaccio/repo' ]; then
+  git clone https://github.com/verdaccio/verdaccio.git ./verdaccio/repo
+fi
+
+pushd ./verdaccio/repo
+git fetch
+git reset --hard
+git clean -fdx
+git checkout master
+git pull
+
+while IFS='' read -r line; do
+  REVISION=$(jq -r '.[0]' <<<"$line")
+  TAGS=$(jq -r '.[1]' <<<"$line")
+
+  git reset --hard
+  git clean -fdx
+  git checkout "$REVISION"
+
   docker buildx build \
+    $TAGS \
     --platform "$PLATFORMS" \
     --pull \
-    --progress plain \
     --push \
-    --build-arg "version=$tag" \
-    --tag "$MIRROR:$tag" \
-    "$@" \
-    ./verdaccio &
-done
-wait
+    $@ \
+    .
+
+done <<<"$BUILDMAP"
+popd
---
+++ buildMap.js
@@ -0,0 +1,88 @@
+const getJSON = (...args) =>
+  new Promise((resolve, reject) => {
+    const transport = args[0].startsWith("https")
+      ? require("https")
+      : require("http");
+
+    transport
+      .get(...args, (res) => {
+        const { statusCode } = res;
+        const contentType = res.headers["content-type"];
+
+        let error;
+        if (statusCode !== 200) {
+          error = new Error("Request Failed.\n" + `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+          error = new Error(
+            "Invalid content-type.\n" +
+              `Expected application/json but received ${contentType}`
+          );
+        }
+
+        if (error) {
+          // consume response data to free up memory
+          res.resume();
+          reject(error);
+          return;
+        }
+
+        res.setEncoding("utf8");
+        let rawData = "";
+        res.on("data", (chunk) => {
+          rawData += chunk;
+        });
+        res.on("end", () => {
+          try {
+            const parsedData = JSON.parse(rawData);
+            resolve(parsedData);
+          } catch (e) {
+            reject(e);
+          }
+        });
+      })
+      .on("error", reject);
+  });
+
+const loadGitHubReleases = (repo) =>
+  getJSON(`https://api.github.com/repos/verdaccio/verdaccio/releases`, {
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "hertzg/docker-verdaccio-multiarch",
+    },
+  });
+
+const isLatestRelease = (release) => !release.prerelease && !release.draft;
+
+const convertReleasesToImageTags = (repo) =>
+  loadGitHubReleases(repo).then((releases) =>
+    releases.reduce(
+      (acc, release) => {
+        if (!acc.latest && isLatestRelease(release)) {
+          acc.latest = release;
+        } else {
+          acc.other.push(release);
+        }
+        return acc;
+      },
+      { latest: null, other: [] }
+    )
+  );
+
+const releaseToTagFlag = (release) => `--tag ${mirror}:${release.tag_name}`;
+
+const convertToBuildMap = ({ latest, other }) => [
+  [latest.tag_name, `--tag ${mirror}:latest ${releaseToTagFlag(latest)}`],
+  ...other.map((release) => [release.tag_name, releaseToTagFlag(release)]),
+];
+
+if (process.argv.length < 4) {
+  console.error(`usage: ${__filename} <origin> <mirror>`);
+  process.exit(1);
+}
+
+const [origin, mirror] = process.argv.slice(2);
+
+convertReleasesToImageTags(origin)
+  .then(convertToBuildMap)
+  .then((entries) => entries.map((entry) => JSON.stringify(entry)).join("\n"))
+  .then(console.log);
